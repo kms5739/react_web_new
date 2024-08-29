c93b1c6b44641afce5d3a4861c6c48c76eeda1ee
kms5739 <minddoli2@naver.com>
2024-08-29 17:59:37 +0900

    header footer 기본화면

--- react_web_front/src/App.js
+++ react_web_front/src/App.js
@@ -1,9 +1,18 @@
 import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/common/Main";
 
 function App() {
   return (
-    <div className="Wrap">
+    <div className="wrap">
       <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
--- react_web_front/src/components/common/Header.js
+++ react_web_front/src/components/common/Header.js
@@ -1,9 +1,51 @@
+import { Link } from "react-router-dom";
+import "./default.css";
+
 const Header = () => {
   return (
-    <div>
-      <h1>헤더</h1>
-    </div>
+    <header className="header">
+      <div>
+        <div className="logo">
+          <Link to="/">KIM`S WORLD</Link>
+        </div>
+        <MainNavi />
+        <HeaderLink />
+      </div>
+    </header>
   );
 };
 
+const MainNavi = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <Link to="#">메뉴1</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴2</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴3</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴4</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const HeaderLink = () => {
+  return (
+    <ul className="user-menu">
+      <li>
+        <Link to="#">로그인</Link>
+      </li>
+      <li>
+        <Link to="#">회원가입</Link>
+      </li>
+    </ul>
+  );
+};
 export default Header;
